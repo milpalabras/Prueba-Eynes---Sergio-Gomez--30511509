ddce9d7cb1b9ccf8adf4780e98afaf0fd0fddc6c
milpalabras <sergio.milpalabras@gmail.com>
2022-07-04 17:10:34 -0300

    agregar metodos magicos para str y mul

--- Clases/clases.py
+++ Clases/clases.py
@@ -7,11 +7,10 @@ class circulo:
         except ValueError as error:
             print(error)
             exit()
-                
+
     @property
     def radio(self):
-        return self._radio
-    
+        return self._radio    
     @radio.setter
     def radio(self, radio):
         self._radio = radio
@@ -21,9 +20,18 @@ class circulo:
         except ValueError as error:
             print(error)
             exit()
-
     
     def area(self):
         return self.radio ** 2 * 3.14
     def perimetro(self):
         return self.radio * 2 * 3.14
+    def __mul__(self, n):
+        try:
+            if n <= 0:
+                raise ValueError("no se puede multiplicar por 0, ingresé un numero mayor que 0")
+        except ValueError as error:
+            print(error)
+            exit()
+        return circulo (self.radio * n)
+    def __str__(self):
+        return "Hola, soy un circulo con un radio de: " + str(self.radio) + ". Mi area es: " + str(self.area()) + " y mi perimetro es: " + str(self.perimetro())
