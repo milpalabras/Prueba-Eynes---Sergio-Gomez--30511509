b33e52674bab43dfdc97aa8a2fcc05b6d27b9e87
milpalabras <sergio.milpalabras@gmail.com>
2022-07-04 17:00:08 -0300

    crear matriz automatica y matriz de test

---
+++ Matriz/matriz.py
@@ -0,0 +1,9 @@
+import random
+
+matriz=[[random.randint(0,30) for i in range(5)] for j in range(5)]
+
+matriz_test = [[1, 2, 3, 4, 0],
+          [2, 3, 4, 16, 0],
+          [3, 4, 5, 0, 0],
+          [4, 3, 2, 1, 0],
+          [5, 4, 3, 2, 1]]
