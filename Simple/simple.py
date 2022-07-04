6ef020b2873b373447bb61f1e2fa359e59103dde
milpalabras <sergio.milpalabras@gmail.com>
2022-07-04 16:54:09 -0300

    creación función ordenar_lista_dict (max y min)

--- Simple/simple.py
+++ Simple/simple.py
@@ -5,3 +5,11 @@ def generar_lista_dict():
     for i in range(1, 11):
         lista.append({'id': i, 'edad': random.randint(1, 100)})
     return lista
+
+def ordenar_lista_dict(lista):
+    print(min(lista, key=lambda x: x['edad'])['id'])
+    print(max(lista, key=lambda x: x['edad'])['id'])
+
+    return sorted(lista, key=lambda x: x['edad'])
+
+ordenar_lista_dict(generar_lista_dict())
