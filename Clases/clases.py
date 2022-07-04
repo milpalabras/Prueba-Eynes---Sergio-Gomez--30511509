dc09d464f8a8f8fd133d381a1fc11d0c6d172144
milpalabras <sergio.milpalabras@gmail.com>
2022-07-04 17:07:48 -0300

    metodo area y perimetro

--- Clases/clases.py
+++ Clases/clases.py
@@ -6,4 +6,8 @@ class circulo:
                 raise ValueError("El radio debe ser mayor que 0")
         except ValueError as error:
             print(error)
-            exit()
+            exit()    
+    def area(self):
+        return self.radio ** 2 * 3.14
+    def perimetro(self):
+        return self.radio * 2 * 3.14
