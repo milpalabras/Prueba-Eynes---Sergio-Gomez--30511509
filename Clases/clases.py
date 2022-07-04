49df5eab9e14a0d1d5e5af11baf5c6b496473b69
milpalabras <sergio.milpalabras@gmail.com>
2022-07-04 17:09:04 -0300

    property y setter para radio mayor que cero

--- Clases/clases.py
+++ Clases/clases.py
@@ -6,7 +6,23 @@ class circulo:
                 raise ValueError("El radio debe ser mayor que 0")
         except ValueError as error:
             print(error)
-            exit()    
+            exit()
+                
+    @property
+    def radio(self):
+        return self._radio
+    
+    @radio.setter
+    def radio(self, radio):
+        self._radio = radio
+        try:
+            if self.radio <= 0:
+                raise ValueError("El radio debe ser mayor que 0")
+        except ValueError as error:
+            print(error)
+            exit()
+
+    
     def area(self):
         return self.radio ** 2 * 3.14
     def perimetro(self):
